953b1657213b85c77e5aff723338dc701c441800
Ahmad Budairi <budairi.contact@gmail.com>
2022-11-08 09:15:19 +0700

    create vuetify helper

---
+++ resources/components/helpers/Pagination.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-pagination
+        v-if="data!=null"
+        v-model="data.current_page"
+        :length="data.last_page"
+        :total-visible="totalVisible"
+        v-show="data.last_page>1"
+        @input="method(data.current_page)"
+    ></v-pagination>
+</template>
+
+<script>
+export default {
+    name: "Pagination",
+    props: {
+        data: {
+            type: Object,
+            default: () => null
+        },
+        method: {type: Function},
+        totalVisible: {
+            type: String,
+            default: () => undefined
+        }
+    },
+    methods: {
+        getData(page) {
+            this.$emit('pagination-change-page', page);
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/components/helpers/Toast.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-snackbar
+        :color="color"
+        v-model="visible"
+        :timeout="1500"
+    >
+        <b v-text="message"/>
+
+    </v-snackbar>
+</template>
+
+<script>
+export default {
+    name: "Toast",
+    data() {
+        return {
+            visible: false,
+            message: null,
+            color: 'success',
+            duration: 1500
+        }
+    },
+    methods: {
+        show(message, color = 's', duration) {
+            this.message = message
+            this.color = color == 's' ? 'green darken-4' : 'red darken-4'
+            this.duration = duration==undefined?1500:duration;
+            this.visible = true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
