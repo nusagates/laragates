4394efb6d122bd738e99fe4cd983d5c700d497d0
Irnovi <huiralb@gmail.com>
2026-01-05 20:24:29 +0700

    fix run db:seed

--- database/seeders/BroadcastDemoSeeder.php
+++ database/seeders/BroadcastDemoSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\BroadcastCampaign;
 use App\Models\BroadcastTarget;
+use App\Models\WhatsappTemplate;
 
 class BroadcastDemoSeeder extends Seeder
 {
@@ -13,9 +14,22 @@ class BroadcastDemoSeeder extends Seeder
         // ID user pertama (biar aman)
         $adminId = \App\Models\User::first()?->id;
 
+        // Create a demo WhatsApp template first
+        $template = WhatsappTemplate::create([
+            'name' => 'promo_akhir_tahun',
+            'category' => 'MARKETING',
+            'language' => 'id',
+            'status' => 'approved',
+            'body' => 'Halo {{1}}, dapatkan diskon hingga 50% untuk pembelian Anda!',
+            'body_params_count' => 1,
+            'created_by' => $adminId,
+            'approved_by' => $adminId,
+            'approved_at' => now(),
+        ]);
+
         $campaign = BroadcastCampaign::create([
             'name' => 'Promo Akhir Tahun',
-            'whatsapp_template_id' => 1,
+            'whatsapp_template_id' => $template->id,
             'audience_type' => 'upload',
             'total_targets' => 10,
             'send_now' => 1,
