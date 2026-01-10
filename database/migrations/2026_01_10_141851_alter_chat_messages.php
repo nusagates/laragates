7f0f4c6dec40c15e0da5d869b0401a3bb17f2fbb
Irnovi <huiralb@gmail.com>
2026-01-10 14:20:14 +0700

    feat(migrations): update chat_messages status column to include additional states

---
+++ database/migrations/2026_01_10_141851_alter_chat_messages.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('chat_messages', function (Blueprint $table) {
+            $table->enum('status', ['pending', 'sent', 'delivered', 'received', 'read', 'failed'])->default('pending')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('chat_messages', function (Blueprint $table) {
+            //
+        });
+    }
+};
